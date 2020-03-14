8f0ba233c9c108302340a5dd533eb3ddd6b313a8
Nickolay Dudaryk <nick.dudaryk@gmail.com>
2020-03-14 14:09:49 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,16 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+    if(!options.separator){
+      options.separator = '+';
+    }
+    if(!options.additionSeparator){
+      options.additionSeparator = '|';
+    }
+    if(!options.additionRepeatTimes && options.repeatTimes){
+      return (str + (options.separator + str).repeat(options.repeatTimes - 1));
+    }
+    if(!options.additionRepeatTimes && !options.repeatTimes){
+      return (str + options.addition);
+    }
+    return (str+((options.addition+options.additionSeparator).repeat(options.additionRepeatTimes - 1))+options.addition)+(options.separator+(str+((options.addition+options.additionSeparator).repeat(options.additionRepeatTimes - 1))+options.addition)).repeat(options.repeatTimes - 1);
+  };
   
